0efd147728dceb9f482bc2b7449fdcf16bcd0e31
Tyler Matteo <tylermatteo434@gmail.com>
2023-04-16 11:19:10 -0400

    Add geospatial index to special purpose districts entity

--- src/special_purpose_districts/special_purpose_district.entity.ts
+++ src/special_purpose_districts/special_purpose_district.entity.ts
@@ -1,7 +1,12 @@
-import { Entity, Property } from '@mikro-orm/core';
+import { Entity, Property, Index } from '@mikro-orm/core';
 import { MultiPolygon } from 'geojson';
 import { MultiPolygonEntity } from '../shared/MultiPolygonEntity';
 
+@Index({
+  name: 'special_purpose_districts_geom_idx',
+  expression:
+    'CREATE INDEX special_purpose_districts_geom_idx on special_purpose_districts USING GIST (geom)',
+})
 @Entity({ tableName: 'special_purpose_districts' })
 export class SpecialPurposeDistrict extends MultiPolygonEntity {
   @Property()
